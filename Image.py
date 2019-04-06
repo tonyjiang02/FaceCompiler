9e867ff519ce5a0109d518130cdab1c260816252
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-04-06 16:18:31 -0700

    Fix image shifting

--- Image.py
+++ Image.py
@@ -1,59 +1,62 @@
 import cv2 as cv2
-import numpy as np
 
 
 class Image:
-        def __init__(self, fileName):
-                def getFace(img):
-                        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-                        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscale
-                        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-                        for (x,y,w,h) in faces: 
-                                return (x,y,w,h)
-                self.fileName = fileName
-                self.image = cv2.imread(fileName)
-                self.Rectangle = getFace(self.image)
+    def __init__(self, fileName):
+        def getFace(img):
+            face_cascade = cv2.CascadeClassifier(
+                'haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # grayscale
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                return (x, y, w, h)
 
-        def getFace(self, img):
-                face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscale
-                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-                for (x,y,w,h) in faces: 
-                        return (x,y,w,h)
+        self.fileName = fileName
+        self.image = cv2.imread(fileName)
+        self.Rectangle = getFace(self.image)
 
-        def update(self):
-                img = self.image
-                face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscale
-                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-                for (x,y,w,h) in faces: 
-                        self.Rectangle = (x,y,w,h)
+    def getFace(self, img):
+        face_cascade = cv2.CascadeClassifier(
+            'haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # grayscale
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            return (x, y, w, h)
 
-        def imgParameters(self):
-                height, width = image.shape[:2]
-                return width, height
+    def update(self):
+        img = self.image
+        face_cascade = cv2.CascadeClassifier(
+            'haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # grayscale
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            self.Rectangle = (x, y, w, h)
 
-        def getMidpoint(self):  # midpoint of face
-                Rectangle = self.Rectangle
-                x = Rectangle[0]
-                y = Rectangle[1]
-                w = Rectangle[2]
-                h = Rectangle[3]
-                return x + w/2, y + h/2
+    def imgParameters(self):
+        height, width = self.image.shape[:2]
+        return width, height
 
-        def getArea(self):  # area of face
-                Rectangle = self.Rectangle
-                w = Rectangle[2]
-                h = Rectangle[3]
-                return w * h
+    def getMidpoint(self):  # midpoint of face
+        Rectangle = self.Rectangle
+        x = Rectangle[0]
+        y = Rectangle[1]
+        w = Rectangle[2]
+        h = Rectangle[3]
+        return x + w / 2, y + h / 2
 
-        def setImage(self, img):
-                self.image = img
+    def getArea(self):  # area of face
+        Rectangle = self.Rectangle
+        w = Rectangle[2]
+        h = Rectangle[3]
+        return w * h
 
-        def showImage(self):
-                cv2.imshow('image',self.image)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-        def saveImage(self):
-                cv2.imwrite(self.fileName+"adjusted", self.image)
+    def setImage(self, img):
+        self.image = img
 
+    def showImage(self):
+        cv2.imshow('image', self.image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def saveImage(self):
+        cv2.imwrite("Adjusted.JPG", self.image)
--- shiftImages.py
+++ shiftImages.py
@@ -1,30 +1,51 @@
-import cv2
-import numpy as np
 from Image import *
-
+import numpy as np
 Tony = "Tony.JPG"
 Luo = "Luo.JPG"
 baseImage = Image(Luo)
 
-#get Area as a ratio of an entire image instead of a ratio of pixels later ....
+
+# get Area as a ratio of an entire image instead of a ratio of pixels later ....
 def getAreaRatio(image1, image2):
-    return image1.getArea()/image2.getArea()
+    return image1.getArea() / image2.getArea()
+
 
 def adjustImage(toAdjust):
     ratio = getAreaRatio(baseImage, toAdjust)
     img = toAdjust.image
-    
-    width = int(img.shape[1] * (ratio**(0.5)))
-    height = int(img.shape[0] * (ratio**(0.5)))
-    img = cv2.resize(img, (width,height))
+
+    width = int(img.shape[1] * (ratio ** (0.5)))
+    height = int(img.shape[0] * (ratio ** (0.5)))
+    img = cv2.resize(img, (width, height))
     toAdjust.setImage(img)
     toAdjust.update()
-    
 
+
+def createImage(baseImage, image1):
+    wh = baseImage.imgParameters()
+    width = wh[0]
+    height = wh[1]
+    blankImage = np.zeros((height, width, 3), np.uint8)
+    midpointBase = baseImage.getMidpoint()
+    midpointImage = image1.getMidpoint()
+
+    xDifference = midpointBase[0] - midpointImage[0]
+    yDifference = midpointBase[1] - midpointImage[1]
+    wh1 = image1.imgParameters()
+    width1 = wh1[0]
+    height1 = wh1[1]
+    print(blankImage.shape)
+    print(image1.image.shape)
+    print(width1, height1)
+    for xcoord in range(0, width):
+        for ycoord in range(0, height):
+            oldx = int(xcoord - xDifference)
+            oldy = int(ycoord - yDifference)
+            if oldy >= 0 and oldx >= 0 and oldy<height1 and oldx<width1:
+                blankImage[ycoord][xcoord] = image1.image[oldy][oldx]
+    cv2.imwrite("Blankspace.JPG", blankImage)
 
 tonyImage = Image(Tony)
 adjustImage(tonyImage)
-print(tonyImage.getArea())
-print(baseImage.getArea())
 tonyImage.saveImage()
-baseImage.saveImage()
+createImage(baseImage,tonyImage)

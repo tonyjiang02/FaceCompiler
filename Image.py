6bbbba974e762394f0f68a9dfe495d03c8c1b364
Tony Jiang <tonyjiang02@gmail.com>
2019-04-06 14:58:01 -0700

    3rd

---
+++ Image.py
@@ -0,0 +1,52 @@
+import cv2 as cv2
+import numpy as np
+
+
+class Image:
+        def __init__(self, fileName):
+                def getFace(img):
+                        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+                        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscale
+                        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+                        for (x,y,w,h) in faces: 
+                                return (x,y,w,h)
+                self.fileName = fileName
+                self.image = cv2.imread(fileName)
+                self.Rectangle = getFace(self.image)
+
+        def getFace(self, img):
+                face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscale
+                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+                for (x,y,w,h) in faces: 
+                        return (x,y,w,h)
+
+        def update(self):
+                self.Rectangle = getFace(self.image)
+
+        def imgParameters(self):
+                height, width = image.shape[:2]
+                return width, height
+
+        def getMidpoint(self):  # midpoint of face
+                Rectangle = self.Rectangle
+                x = Rectangle[0]
+                y = Rectangle[1]
+                w = Rectangle[2]
+                h = Rectangle[3]
+                return x + w/2, y + h/2
+
+        def getArea(self):  # area of face
+                Rectangle = self.Rectangle
+                w = Rectangle[2]
+                h = Rectangle[3]
+                return w * h
+
+        def setImage(self, img):
+                self.image = img
+
+        def showImage(self):
+                cv2.imshow('image',self.image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+
--- shiftImages.py
+++ shiftImages.py
@@ -1,5 +1,32 @@
 import cv2 as cv2
 import numpy as np
-from getMidpoint.py import *
+from Image import *
+
+Tony = "Tony.JPG"
+Luo = "Luo.JPG"
+baseImage = Image(Luo)
+
+def resize(image):
+    height, width = image.shape[:2]
+    return cv2.resize(image,(width,height))
+
+#get Area as a ratio of an entire image instead of a ratio of pixels later ....
+def getAreaRatio(image1, image2):
+    return image1.getArea()/image2.getArea()
+
+def adjustImage(toAdjust):
+    print(toAdjust.getArea())
+    ratio = getAreaRatio(toAdjust, baseImage)
+    img = toAdjust.image
+    
+    width = int(img.shape[1] * ratio)
+    height = int(img.shape[0] * ratio)
+    img = cv2.resize(img, (width,height))
+    
+    toAdjust.setImage(img)
+    toAdjust.update()
+    print(toAdjust.getArea())
 
 
+tonyImage = Image(Tony)
+adjustImage(tonyImage)
